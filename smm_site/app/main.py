8b582f7dbbea7a013f6e7d6d64f14a43bfbb6cc4
iusgiuwrugiav345 <matersasm@gmail.com>
2025-12-30 18:07:17 +0500

    Add files via upload

---
+++ smm_site/app/main.py
@@ -0,0 +1,13 @@
+from fastapi import FastAPI
+from fastapi.staticfiles import StaticFiles
+
+# Import routes using package path
+from app.routes import site
+
+app = FastAPI()
+
+# Статика для CSS/JS
+app.mount("/static", StaticFiles(directory="app/static"), name="static")
+
+# Подключаем маршруты
+app.include_router(site.router)
